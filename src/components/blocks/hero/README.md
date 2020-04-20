c148e68db5a8dca5490e9684ef340b5491e36f82
Trevor Morris <code@trovster.com>
2020-04-20 15:23:20 +0100

    fix: add hero block

---
+++ src/components/blocks/hero/README.md
@@ -0,0 +1,3 @@
+## Hero
+
+Introduction hero block.
---
+++ src/components/blocks/hero/hero.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  order: 12,
+  title: 'Hero',
+  handle: 'block--hero',
+  status: 'wip',
+  default: 'default',
+  context: {
+    text: 'Woods for sale for conservation and enjoyment.'
+  }
+}
