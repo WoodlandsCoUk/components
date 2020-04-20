ad61851ba7372d32048670be8bf3003950ccdb46
Trevor Morris <code@trovster.com>
2020-04-20 10:57:48 +0100

    refactor: dna for development and glob include scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,37 @@
+const globImporter = require('node-sass-glob-importer')
 const StylelintPlugin = require('stylelint-webpack-plugin')
 
 module.exports = {
-  module: {},
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: process.env.NODE_ENV === 'development',
+              prependData: () => {
+                const data = [
+                  '$env: ' + process.env.NODE_ENV + ';'
+                ]
+
+                if (process.env.NODE_ENV === 'development') {
+                  data.push('@import "../components/dna/**/*.scss";')
+                }
+
+                return data.join('')
+              },
+              sassOptions: {
+                outputStyle: process.env.NODE_ENV === 'development' ? 'expanded' : 'compressed',
+                importer: globImporter()
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
   plugins: [
     new StylelintPlugin({
       files: 'src/sass/site.scss'
