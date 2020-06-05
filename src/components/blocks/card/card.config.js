33a4470acbccd9f9c694a56ac6a19ad7f56542f4
Trevor Morris <code@trovster.com>
2020-06-05 12:41:28 +0100

    fix: card styling

--- src/components/blocks/card/card.config.js
+++ src/components/blocks/card/card.config.js
@@ -11,7 +11,6 @@ module.exports = {
     card: {
       title: 'Default Card',
       text: faker.lorem.paragraph(),
-      category: faker.address.county(),
       media: {
         src: faker.image.imageUrl(400, 225, 'nature', true)
       }
@@ -29,8 +28,7 @@ module.exports = {
         card: {
           title: 'Card with Banner',
           text: faker.lorem.paragraph(),
-          category: faker.address.county(),
-          banner: new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'GBP' }).format(faker.random.number()).slice(0, -3)
+          banner: 'Banner'
         }
       }
     },
@@ -41,7 +39,10 @@ module.exports = {
         card: {
           title: 'Card with Video',
           text: faker.lorem.paragraph(),
-          video: 'http://www.youtube.com/embed/dP15zlyra3c?html5=1'
+          media: {
+            video: 'http://www.youtube.com/embed/dP15zlyra3c?html5=1',
+            modifiers: ['video']
+          }
         }
       }
     },
@@ -52,7 +53,24 @@ module.exports = {
         card: {
           title: 'Card with Link',
           text: faker.lorem.paragraph(),
-          link: 'https://www.woodlands.co.uk'
+          link: '#link'
+        }
+      }
+    },
+    {
+      name: 'woodland',
+      label: 'Woodland',
+      context: {
+        card: {
+          title: 'Woodland Card',
+          text: 'About 5 ½ acres',
+          link: 'page-wood',
+          location: [
+            `NR. ${faker.address.streetName()}`,
+            faker.address.city(),
+            faker.address.county()
+          ].join(', '),
+          banner: new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'GBP' }).format(faker.random.number()).slice(0, -3)
         }
       }
     }
