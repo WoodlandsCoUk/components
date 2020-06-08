240112e2faad3b984b15664bb8665e7bd2906ff2
Trevor Morris <code@trovster.com>
2020-06-08 13:43:49 +0100

    fix: woodland hero extra

--- src/components/blocks/hero/hero.config.js
+++ src/components/blocks/hero/hero.config.js
@@ -1,3 +1,5 @@
+const faker = require('faker')
+
 module.exports = {
   order: 20,
   title: 'Hero',
@@ -6,5 +8,19 @@ module.exports = {
   default: 'default',
   context: {
     text: 'Woods for sale for conservation and enjoyment.'
-  }
+  },
+  variants: [
+    {
+      name: 'default',
+      label: 'Default'
+    },
+    {
+      name: 'woodland',
+      label: 'Woodland',
+      context: {
+        text: faker.lorem.words(3),
+        extra: new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'GBP' }).format(faker.random.number()).slice(0, -3),
+      }
+    }
+  ]
 }
