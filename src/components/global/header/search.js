ae62811041fd97efb07bb8dc20876b5e158f4b05
Trevor Morris <code@trovster.com>
2020-07-10 14:23:04 +0100

    fix: js check element exists for search and nav

--- src/components/global/header/search.js
+++ src/components/global/header/search.js
@@ -1,12 +1,15 @@
 const header = document.querySelector('.header')
-const navigationSearch = header.querySelector('.form--search:not(.form--search--simple)')
-const navigationSearchToggle = header.querySelector('.js--navigation-search')
 
-if (navigationSearch && navigationSearchToggle) {
-  navigationSearchToggle.addEventListener('click', (event) => {
-    event.preventDefault()
+if (header) {
+  const navigationSearch = header.querySelector('.form--search:not(.form--search--simple)')
+  const navigationSearchToggle = header.querySelector('.js--navigation-search')
 
-    navigationSearch.classList.toggle('is-shown')
-    navigationSearch.querySelector('input').focus()
-  })
+  if (navigationSearch && navigationSearchToggle) {
+    navigationSearchToggle.addEventListener('click', (event) => {
+      event.preventDefault()
+
+      navigationSearch.classList.toggle('is-shown')
+      navigationSearch.querySelector('input').focus()
+    })
+  }
 }
