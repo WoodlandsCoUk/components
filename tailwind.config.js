8cedf027d40874e5e7312a7dbb755887022328b4
Trevor Morris <code@trovster.com>
2020-07-17 11:38:15 +0100

    fix: implement font variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,12 @@ module.exports = {
     fontFamily: {
       display: ['FreightNeo Pro Bold', 'FreightNeoProBold-Regular', 'serif'],
       body: ['Sofia Pro Medium', 'SofiaPro-Medium', 'sans-serif'],
-      quote: ['FreightNeo Pro Semi-Italic', 'FreightNeoProSemi-Italic', 'serif']
+      quote: ['FreightNeo Pro Semi Italic', 'FreightNeo Pro Semi-Italic', 'FreightNeoProSemi-Italic', 'serif'],
+      'body-italic': ['Sofia Pro Medium Italic', 'Sofia Pro MediumItalic', 'SofiaPro-Mediumitalic', 'sans-serif'],
+      'body-semi-bold': ['Sofia Pro Semi-Bold', 'Sofia Pro SemiBold', 'SofiaPro-SemiBold', 'sans-serif'],
+      'body-bold': ['Sofia Pro Bold', 'SofiaPro-Bold', 'sans-serif'],
+      'body-bold-italic': ['Sofia Pro Bold Italic', 'Sofia Pro BoldItalic', 'SofiaPro-Bolditalic', 'sans-serif'],
+      'body-black': ['Sofia Pro Black', 'SofiaPro-Black', 'sans-serif']
     },
     colors: {
       transparent: 'transparent',
