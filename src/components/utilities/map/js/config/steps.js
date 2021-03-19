019c2262d4013249b67761b40733966e32e0c25f
Tom Ward <tom@woodlands.co.uk>
2021-03-19 15:36:09 +0000

    fix: comments should match the code

--- src/components/utilities/map/js/config/steps.js
+++ src/components/utilities/map/js/config/steps.js
@@ -3,16 +3,16 @@ module.exports = {
   small: {
     at: 5,
     size: 20,
-    color: '#92a892' // blue
+    color: '#92a892' // brand-map-hover green
   },
   medium: {
     at: 10,
     size: 30,
-    color: '#92a892' // yellow
+    color: '#92a892' // brand-map-hover green
   },
   large: {
     at: 15,
     size: 40,
-    color: '#92a892' // red
+    color: '#92a892' // brand-map-hover green
   }
 }
