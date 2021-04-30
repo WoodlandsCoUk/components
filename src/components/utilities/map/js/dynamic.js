d69841f0868ef83b1eac633c69556aef51eaea63
Tom Ward <tom@woodlands.co.uk>
2021-04-30 16:37:01 +0100

    feat: add another mouseover on the map view
    * in this case, show the wood title on mouseover of a wood marker
    * I think it clashes a bit with the UI of clicking on a wood marker for the long description, but the agents specifically asked for this

--- src/components/utilities/map/js/dynamic.js
+++ src/components/utilities/map/js/dynamic.js
@@ -182,12 +182,33 @@ maps.forEach(container => {
       mapElement.getCanvas().style.cursor = ''
     })
 
-    mapElement.on('mouseenter', 'woodlands-map-detail', () => {
+    const titlePopup = new System.Popup({
+      ...popupObject,
+      closeOnClick: false
+    })
+
+    mapElement.on('mouseenter', 'woodlands-map-detail', (event) => {
+      const coordinates = event.features[0].geometry.coordinates.slice()
+      const description = event.features[0].properties.title
+
       mapElement.getCanvas().style.cursor = 'pointer'
+
+      // Ensure that if the map is zoomed out such that
+      // multiple copies of the feature are visible, the
+      // popup appears over the copy being pointed to.
+      while (Math.abs(event.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += event.lngLat.lng > coordinates[0] ? 360 : -360
+      }
+
+      titlePopup
+        .setLngLat(coordinates)
+        .setHTML(description)
+        .addTo(mapElement)
     })
 
     mapElement.on('mouseleave', 'woodlands-map-detail', () => {
       mapElement.getCanvas().style.cursor = ''
+      titlePopup.remove()
     })
 
     const countPopup = new System.Popup({
