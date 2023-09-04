e9bd7e05fbb334443085792b5dbe626701ccbf61
Tom Ward <tomward@fmail.co.uk>
2023-09-04 13:36:12 +0000

    feat: add an example textarea
    * want to use this for the community woodlands application form

---
+++ src/components/utilities/form/textarea/textarea.config.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+  collated: true,
+  order: 20,
+  title: 'Text field area',
+  label: 'TextArea',
+  handle: 'form--textarea',
+  status: 'wip',
+  default: 'default',
+  context: {
+    name: 'example-textarea'
+  },
+  variants: [
+    {
+      name: 'default',
+      label: 'Default'
+    }
+  ]
+}
