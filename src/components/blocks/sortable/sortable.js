0f6ee06601d2c04e337713fb88d96555e2d5df2e
Tom Ward <tomward@fmail.co.uk>
2026-06-05 12:14:18 +0000

    feat(admin_ui): implement callback for wood particulars photo ui reorder

--- src/components/blocks/sortable/sortable.js
+++ src/components/blocks/sortable/sortable.js
@@ -2,6 +2,37 @@ import Sortable from 'sortablejs'
 
 const sortableGalleries = document.querySelectorAll('.gallery__sortable')
 
-sortableGalleries.forEach((sortableGallery) => {
-  Sortable.create(sortableGallery)
+// TODO: this is a bit tightly coupled to the laravel app for my taste rn -
+// plz refactor
+sortableGalleries.forEach(sortableGallery => {
+  if (
+    sortableGallery.dataset.callback &&
+    sortableGallery.dataset.callback === 'photo'
+  ) {
+    Sortable.create(sortableGallery, {
+      onUpdate: function (e) {
+        console.log('move photo', e.oldIndex, 'to', e.newIndex)
+        const req = new XMLHttpRequest() // eslint-disable-line
+        req.open(
+          'POST',
+          '/admin_new/wood_photos/move/' +
+            e.to.dataset.woodid +
+            '/' +
+            e.oldIndex +
+            '/' +
+            e.newIndex
+        )
+        req.setRequestHeader('x-csrf-token', sortableGallery.dataset.csrf)
+        req.setRequestHeader('Content-Type', 'application/json; charset=utf-8')
+        req.setRequestHeader('Accept', 'application/json')
+        req.send()
+      }
+    })
+  } else {
+    Sortable.create(sortableGallery, {
+      onUpdate: function (e) {
+        console.log(e.oldIndex, e.newIndex)
+      }
+    })
+  }
 })
